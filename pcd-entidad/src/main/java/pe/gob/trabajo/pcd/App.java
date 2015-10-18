94bae7c5dc7d4bced99c9f2fb74d5ca850c15e48
hever <hpumallihua@gmail.com>
2015-10-18 17:14:20 -0500

    cambios pedido

--- pcd-entidad/src/main/java/pe/gob/trabajo/pcd/App.java
+++ pcd-entidad/src/main/java/pe/gob/trabajo/pcd/App.java
@@ -10,5 +10,10 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
+        
+        
+        double div2 = (double)3 / 100;
+        
+        System.out.println( "Hello World=" +div2 );
     }
 }
